f24daafe40231661e0155440a3bcc17acbc2cf2d
tangmack <macktang@gmail.com>
2021-02-28 08:46:15 -0700

    detecting four corners except when frames blurry

--- detection_video.py
+++ detection_video.py
@@ -29,8 +29,8 @@ if __name__ == '__main__':
 
     if use_video == True:
         # cap = cv2.VideoCapture('Tag0.mp4')
-        cap = cv2.VideoCapture('Tag1.mp4')
-        # cap = cv2.VideoCapture('Tag2.mp4')
+        # cap = cv2.VideoCapture('Tag1.mp4')
+        cap = cv2.VideoCapture('Tag2.mp4')
         # cap = cv2.VideoCapture('multipleTags.mp4')
     else:
         single_image = cv2.imread('frame0_Tag1_color.png', 0)
@@ -80,8 +80,9 @@ if __name__ == '__main__':
 
         # continue
 
-        '''#############################################################################################'''
+        '''########### Get second biggest contour within paper only (first biggest is paper itself) ################'''
         # todo for 3 ar tags, just pick 3 biggest instead of THE biggest, and then do same procedure FOR EACH
+        # todo for 3 ar tags, also change goodFeaturesToTrack number of points to look for (efficiency)
         # todo track centroid between images, verify movement was not too much
 
         '''Get all contours within big contour mask (faster)'''
@@ -92,19 +93,102 @@ if __name__ == '__main__':
                     removearray(contours,c)
                     break
 
-        # Purely visualization:
-        color_img_only_within_paper_contours = frame.copy()
-        cv2.drawContours(color_img_only_within_paper_contours, contours, 1, colors[2], thickness=cv2.FILLED)
-        print(frame_count)
-        cv2.imshow('frame', color_img_only_within_paper_contours)
-        ## cv2.waitKey(0)
+        # # Purely visualization:
+        # color_img_only_within_paper_contours = frame.copy()
+        # cv2.drawContours(color_img_only_within_paper_contours, contours, 1, colors[2], thickness=cv2.FILLED)
+        # print(frame_count)
+        # cv2.imshow('frame', color_img_only_within_paper_contours)
+        # ## cv2.waitKey(0)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
+        '''##################################################################################################'''
+
+        ''' Pick second largest remaining body'''
+        ar_contour = contours[1]
+        ar_mask = np.zeros(img.shape, dtype=np.uint8)
+        cv2.drawContours( ar_mask , contours, 1, 1, thickness=cv2.FILLED)
+
+        # # Purely for visualization (need to change above drawing color from 1 to 255 for this to work)
+        # cv2.imshow('frame', ar_mask)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
+        '''######################## Find and annotate best 4 corners (This module is slow) ##########################'''
+
+        ''' grow/dilate ''' # todo remove?
+        # kernel = np.ones((5, 5), np.uint8)
+        # dilation = cv2.dilate(ar_mask, kernel, iterations=1)
+
+        '''run corner detection'''
+        corners = cv2.goodFeaturesToTrack(img, 25, .1, 25) # option 1: slower corner deteciton
+        corners = np.int0(corners)
+
+        # dst = cv2.cornerHarris(img, 3, 3, 0.2) # option 2: faster corner detection
+        # # corners_tuple = np.where(dst > 0.1 * dst.max())
+        # corners_tuple = np.where(dst > 0.01 * dst.max())
+        # corners_x = corners_tuple[1].reshape(-1, 1)
+        # corners_y = corners_tuple[0].reshape(-1, 1)
+        # corners = np.hstack((corners_x, corners_y))
+
+
+        # for i in corners: # show all detected corners
+        #     x, y = i.ravel()
+        #     cv2.circle(img, (x, y), 3, 255, -1)
+        # plt.imshow(img), plt.show()
+
+        ''' mask corners with AR mask '''
+        valid_corners = []
+        for i in corners:
+            x, y = i.ravel()
+            if ar_mask[y, x] > 0:
+                valid_corners.append(i)
+
+        ''' Display image with valid corners '''
+        # # Purely for visualization
+        # color_image_all_corners = frame.copy()
+        # for idx, i in enumerate(valid_corners):
+        #     x, y = i.ravel()
+        #     cv2.circle(color_image_all_corners, (x, y), 3, colors[idx % 3], 1)
+        # cv2.imshow('frame', color_image_all_corners)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
+        # continue
+
+        ''' Pick 4 points furthest from centroid of ar mask'''
+        M = cv2.moments(ar_mask) # calculate moments of binary image
+
+        cX = int(M["m10"] / M["m00"]) # calculate x,y coordinate of center
+        cY = int(M["m01"] / M["m00"])
+
+        distances = []
+        for i in valid_corners:
+            x, y = i.ravel()
+            d = math.sqrt((y - cY) ** 2 + (x - cX) ** 2)
+            distances.append(d)
+
+        n_distances = np.array(distances)
+        n_valid_corners = np.array(valid_corners)
+        inds = n_distances.argsort()
+        sorted_n_valid_corners = n_valid_corners[inds]
+
+        best_corners = list(sorted_n_valid_corners.reshape(-1, 2))[::-1][0:4]
+
+        '''Display best 4 corners'''
+        # Purely for visualization
+        color_image_best_corners = frame.copy()
+        for idx, i in enumerate(best_corners):
+            x, y = i.ravel()
+            cv2.circle(color_image_best_corners, (x, y), 9, colors[idx % 3], -1)
+        cv2.imshow('frame', color_image_best_corners)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        # ''' Pick second largest remaining body'''
-        # ar_contour = contours[1]
-        # ar_mask = masks[1]
+        '''##################################################################################################'''
 
+
+        # plt.imshow(original2), plt.show()
 
         # '''Get all contours within big contour mask'''
         # # convert all to np arrays 0 to 1
@@ -114,7 +198,7 @@ if __name__ == '__main__':
         # masks = []
         # for idx, c in enumerate(contours):
         #     a = blank_slate.copy()
-        #     cv2.drawContours(a, contours, idx, 1, thickness=cv2.FILLED)  # todo what does thickness do?
+        #     cv2.drawContours(a, contours, idx, 1, thickness=cv2.FILLED)
         #     masks.append(a)
         #
         # # get area of original contours
